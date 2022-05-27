403b56e649f34ecbc53a3a02de0d17fbb612852f
TuckerN-Code <nultytucker@gmail.com>
2022-05-27 10:48:25 -0700

    Form input mostly working

--- LeagueCalendar/app/controllers/wl_cals_controller.rb
+++ LeagueCalendar/app/controllers/wl_cals_controller.rb
@@ -1,14 +1,11 @@
-require "lol_cal"
+require 'lol_cal'
 
 class WlCalsController < ApplicationController
-    def index
-      @days = WlCal::all_days()
-    end
 
-    def update_calendar
-    end
-
-    def cal_params 
-      params.require(:cals).permit(:month).to_h
-    end
+  def index
+    @days = WlCal::all_days
+    @month = params[:month]
   end
+
+
+end
--- LeagueCalendar/app/models/wl_cal.rb
+++ LeagueCalendar/app/models/wl_cal.rb
@@ -1,14 +1,11 @@
 require "lol_cal"
 
 class WlCal < ApplicationRecord
-    def self.all_days
+       def self.all_days
         #days = LolCal::daysInfo
         days = [Day.new("50", "red", 1), Day.new("80", "Green", 2)]
     end
 
-    def self.returnCal(monthIn)
-        month = monthIn
-    end
 end
 
 Day = Struct.new(:winrate, :color, :dayNumber) do
